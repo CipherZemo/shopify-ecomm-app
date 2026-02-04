bf62595a5c177111aa87345f2d1fd0754f14620d
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-04 22:36:01 +0530

    app entry point

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
 dotenv.config();
+connectDB();
+
 
 const app = express();
-
-// middleware to read JSON
 app.use(express.json());
 
-// test route
-app.get("/", (req, res) => {
-  res.send("E-Commerce backend is running 🚀");
-});
+
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/test", require("./routes/testRoute"));
+
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
