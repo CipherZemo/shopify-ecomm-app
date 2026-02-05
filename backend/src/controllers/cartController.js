61cecc0f0d6cef4e201ddb5bff6d8a5b795e0a33
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-05 10:36:29 +0530

    cart controller

---
+++ backend/src/controllers/cartController.js
@@ -0,0 +1,60 @@
+const Cart = require("../models/Cart");
+const Product = require("../models/Product");
+
+// get user cart
+exports.getCart = async (req, res) => {
+  const cart = await Cart.findOne({ user: req.user._id }).populate(
+    "items.product"
+  );
+  res.json(cart || { items: [] });
+};
+
+// add/update item
+exports.addToCart = async (req, res) => {
+  const { productId, quantity } = req.body;
+
+  const product = await Product.findById(productId);
+  if (!product) {
+    return res.status(404).json({ message: "Product not found" });
+  }
+
+  let cart = await Cart.findOne({ user: req.user._id });
+
+  if (!cart) {
+    cart = await Cart.create({
+      user: req.user._id,
+      items: [{ product: productId, quantity }],
+    });
+  } else {
+    const item = cart.items.find(
+      (i) => i.product.toString() === productId
+    );
+
+    if (item) {
+      item.quantity = quantity;
+    } else {
+      cart.items.push({ product: productId, quantity });
+    }
+
+    await cart.save();
+  }
+
+  res.json(cart);
+};
+
+// remove item
+exports.removeFromCart = async (req, res) => {
+  const { productId } = req.params;
+
+  const cart = await Cart.findOne({ user: req.user._id });
+  if (!cart) {
+    return res.status(404).json({ message: "Cart not found" });
+  }
+
+  cart.items = cart.items.filter(
+    (i) => i.product.toString() !== productId
+  );
+
+  await cart.save();
+  res.json(cart);
+};
