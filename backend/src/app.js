85af2028b1b69991b28682b078eba46f7773e5a0
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-06 23:02:31 +0530

    socket setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const http = require("http");
+const { Server } = require("socket.io");
 const connectDB = require("./config/db");
+const socketUtil = require("./utils/socket");
+socketUtil.init(io);
 
 dotenv.config();
 connectDB();
@@ -10,12 +14,23 @@ const app = express();
 app.use(express.json());
 
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/test", require("./routes/testRoute"));
+app.use("/api/auth", require("./routes/authRoutes"));   
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/cart", require("./routes/cartRoutes"));
 app.use("/api/wishlist", require("./routes/wishlistRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/payments", require("./routes/paymentRoutes"));
 
+const server = http.createServer(app);
+
+// socket setup
+const io = new Server(server, {
+  cors: {
+    origin: "*", // later restrict to frontend URL
+  },
+});
+
+require("./sockets")(io);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
+server.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
