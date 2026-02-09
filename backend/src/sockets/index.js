1a8806b3339a01d4af60fb344ae5a8c444cda4d7
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-09 21:51:08 +0530

    chatbot socket

--- backend/src/sockets/index.js
+++ backend/src/sockets/index.js
@@ -1,24 +1,31 @@
+const Chat = require("../models/Chat");
+const chatbotService = require("../services/chatbotService");
+
 module.exports = (io) => {
   io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
-
-    // join user-specific room
     socket.on("join", (userId) => {
       socket.join(userId);
-      console.log(`User ${userId} joined room`);
     });
 
-    // chat message
-    socket.on("chatMessage", (data) => {
-      const { userId, message } = data;
-
-      io.to(userId).emit("chatReply", {
-        reply: `You said: ${message}`,
+    socket.on("chatMessage", async ({ user, message }) => {
+      await Chat.create({
+        user: user._id,
+        sender: "user",
+        message,
       });
-    });
 
-    socket.on("disconnect", () => {
-      console.log("User disconnected:", socket.id);
+      const reply = await chatbotService.getBotReply(
+        user,
+        message
+      );
+
+      await Chat.create({
+        user: user._id,
+        sender: "bot",
+        message: reply,
+      });
+
+      io.to(user._id.toString()).emit("chatReply", { reply });
     });
   });
 };
