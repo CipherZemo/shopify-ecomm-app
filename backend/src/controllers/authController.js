a106e018eac709c593da869771abf260ddeed09a
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-12 20:54:14 +0530

    updated response to get user.id

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -54,6 +54,12 @@ exports.login = async (req, res) => {
     res.json({
       message: "Login successful",
       token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
