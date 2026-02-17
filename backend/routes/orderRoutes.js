8f9dbeabb6df4b1356fdc362aeb9f3bcc9c40f7f
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-17 13:48:14 +0530

    order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
-const { createOrder, getMyOrders, getAllOrders, updateOrderStatus } = require("../controllers/orderController");
+const { createOrder, cancelOrder, getMyOrders, getAllOrders, updateOrderStatus } = require("../controllers/orderController");
 
 const router = express.Router();
 
 // user routes
 router.post("/", protect, createOrder);
+router.put("/:id/cancel", protect, cancelOrder);
 router.get("/my", protect, getMyOrders);
 
 // admin routes
