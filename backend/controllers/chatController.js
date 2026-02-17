0a0093681eaec63fafbed0444a7a4f4cab3c08cd
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-17 13:46:18 +0530

    chatbot controller

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,36 +1,40 @@
 const Chat = require("../models/Chat");
 const chatbotService = require("../services/chatbotService");
 
-exports.sendMessage = async (req, res) => {
-  const { message } = req.body;
+// exports.sendMessage = async (req, res) => {
+//   const { message } = req.body;
 
-  // Save user message
-  await Chat.create({
-    user: req.user._id,
-    sender: "user",
-    message,
-  });
+//   // Save user message
+//   await Chat.create({
+//     user: req.user._id,
+//     sender: "user",
+//     message,
+//   });
 
-  // Get bot reply
-  const reply = await chatbotService.getBotReply(
-    req.user,
-    message
-  );
+//   // Get bot reply
+//   const reply = await chatbotService.getBotReply(
+//     req.user,
+//     message
+//   );
 
-  // Save bot reply
-  await Chat.create({
-    user: req.user._id,
-    sender: "bot",
-    message: reply,
-  });
+//   // Save bot reply
+//   await Chat.create({
+//     user: req.user._id,
+//     sender: "bot",
+//     message: reply,
+//   });
 
-  res.json({ reply });
-};
+//   res.json({ reply });
+// };
 
-exports.getChatHistory = async (req, res) => {
-  const chats = await Chat.find({ user: req.user._id }).sort({
-    createdAt: 1,
-  });
+exports.getChat = async (req, res) => {
+  try {
+    const chats = await Chat.find({ user: req.user._id }).sort({
+      createdAt: 1,
+    });
 
-  res.json(chats);
+    res.json(chats);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
