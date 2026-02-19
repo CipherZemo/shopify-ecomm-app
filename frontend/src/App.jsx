4fc45c5e695f462244cefde36440961d2ca79d75
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-19 21:03:29 +0530

    routes for cart and wishlsit pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getProfile } from './store/slices/authSlice';
 import Navbar from './components/Navbar';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
 import ProductDetailPage from './pages/ProductDetailPage';
+import CartPage from './pages/CartPage';
+import WishlistPage from './pages/WishlistPage';
 
 const ProtectedRoute = ({ children }) => {
   const { token } = useSelector((state) => state.auth);
@@ -18,6 +22,16 @@ const AuthRoute = ({ children }) => {
 };
 
 function App() {
+    const dispatch = useDispatch();
+  const { token } = useSelector((state) => state.auth);
+
+  // ⭐ Fetch user profile on app load if token exists
+  useEffect(() => {
+    if (token) {
+      dispatch(getProfile());
+    }
+  }, [token, dispatch]);
+
   return (
     <BrowserRouter>
       <Navbar />
@@ -25,6 +39,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/cart" element={<ProtectedRoute><CartPage /></ProtectedRoute>} />
+        <Route path="/wishlist" element={<ProtectedRoute><WishlistPage /></ProtectedRoute>} />
         <Route path="/login" element={<AuthRoute><LoginPage /></AuthRoute>} />
         <Route path="/register" element={<AuthRoute><RegisterPage /></AuthRoute>} />
       </Routes>
