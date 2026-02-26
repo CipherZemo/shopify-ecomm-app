454b89402149fa92c60b97f9c377077f0a25134c
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-26 15:50:30 +0530

    injected axios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store/store'
+import { injectStore } from './api/axios';
 import './index.css'
 import App from './App.jsx'
+
+injectStore(store);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
